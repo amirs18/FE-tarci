2b214cf5d2deb9ebcbd0a2a8caca5df68283f066
amirs18 <amir135246@gmail.com>
2024-07-26 13:45:02 +0000

    added indusris culum

--- src/components/SMBSData.tsx
+++ src/components/SMBSData.tsx
@@ -5,16 +5,26 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 const columns: GridColDef[] = [
   { field: "name", headerName: "name", width: 250 },
   { field: "country", headerName: "country", width: 250 },
+  { field: "industries", headerName: "industries", width: 400 },
 ];
 
 function SMBSData({ naicsCode }: { naicsCode: number | undefined }) {
   const { data, isLoading } = useSMBS(naicsCode);
+  const rows = data?.data.map((d) => {
+    return {
+      ...d,
+      industries: d.industries.reduce(
+        (acc, cur, i) => (acc += cur.name + ", "),
+        ""
+      ),
+    };
+  });
 
   return (
     <div style={{ height: 370 }}>
       <DataGrid
         columns={columns}
-        rows={data?.data}
+        rows={rows}
         getRowId={(row) => row.name + row.country}
         autoPageSize={true}
       />
