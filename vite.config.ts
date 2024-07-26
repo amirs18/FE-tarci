58fbc89c262124858a6e670322d4420eb857a7f0
amirs18 <amir135246@gmail.com>
2024-07-26 10:25:53 +0000

    added tailwind and daysi ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import MillionLint from "@million/lint";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+const _plugins = [MillionLint.vite(), react()];
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: _plugins,
+});
